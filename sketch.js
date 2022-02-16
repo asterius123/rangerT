008388be7671b95ff723af5098f90248666e3707
asterius123 <89555646+asterius123@users.noreply.github.com>
2022-02-16 16:05:03 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
-var map , Cdoge , Doge , dogeImage;
+var map ;
+var Cdoge , dogeImage;
+var FireW , FireWImage , MagiB , MagiBImage , MagiG , MagiGImage , MagiM , MagiMImage , MagiP , MagiPImage , MagiR , MagiRImage , MagiS , MagiSImage , MagiY , MagiYImage;
 function preload() {
   map = loadImage("map1.png");
   dogeImage = loadImage("Rangers/comaDoge.png");
+  MagiBImage = loadImage("Rangers/MagiBlue.png");
+  MagiGImage = loadImage("Rangers/MagiGreen.png");
+  MagiMImage = loadImage("Rangers/MagiMom.png");
+  MagiPImage = loadImage("Rangers/MagiPink.png");
+  MagiRImage = loadImage("Rangers/MagiRed.png");
+  MagiSImage = loadImage("Rangers/MagiShine.png");
+  MagiYImage = loadImage("Rangers/MagiYellow.png");
 }
 function setup() {
   createCanvas(1500,800);
-  Cdoge = new Herois(278,350);
+  Cdoge = new Herois(278,300);
   Cdoge.sprite.addImage(dogeImage);
 }
 
